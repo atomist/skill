44f64635fa2b9f39d45c0dc4de74f8f86285e6fa
Christian Dupuis <cd@atomist.com>
2020-08-19 11:22:58 +0200

    Fix test

--- test/definition/subscription/named.test.ts
+++ test/definition/subscription/named.test.ts
@@ -1,3 +1,19 @@
+/*
+ * Copyright © 2020 Atomist, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import * as assert from "assert";
 import * as fs from "fs";
 import * as path from "path";
@@ -5,7 +21,7 @@ import { named } from "../../../lib/definition/subscription/named";
 
 describe("named", () => {
 	it("should load onNewChatUser subscription", () => {
-		const subscription = named("@atomist/skill/chat/onNewChatUser");
+		const subscription = named("@atomist/skill/chat/onNewUser");
 		assert.deepStrictEqual(
 			subscription,
 			fs
@@ -15,7 +31,7 @@ describe("named", () => {
 						"graphql",
 						"subscription",
 						"chat",
-						"onNewChatUser.graphql",
+						"onNewUser.graphql",
 					),
 				)
 				.toString(),
