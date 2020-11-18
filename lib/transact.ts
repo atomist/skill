e62ad7db574d570ab4e1f81add018342a732aec6
Christian Dupuis <cd@atomist.com>
2020-11-18 17:25:52 +0100

    Add support for nesting

--- lib/transact.ts
+++ lib/transact.ts
@@ -3,10 +3,7 @@ import { toEDNStringFromSimpleObject } from "edn-data";
 import { debug, error } from "./log/console";
 import { replacer, toArray } from "./util";
 
-export type Entity<E extends string> = Record<
-	E,
-	Record<string, string | number | boolean>
->;
+export type Entity<E extends string> = Record<E, Record<string, any>>;
 export type Transact<E extends string> = (
 	entities: Entity<E> | Entity<E>[],
 ) => Promise<void>;
@@ -48,11 +45,44 @@ export function createTransact(
 export function flattenEntities(
 	entities: Entity<any> | Entity<any>[],
 ): Record<string, any>[] {
-	return toArray(entities).map(e => {
+	// Flatten nested objects
+	const flattenedEntities = [...toArray(entities)];
+	let counter = 0;
+	toArray(entities).forEach(e => {
 		const entityType = Object.keys(e)[0];
+		for (const key of Object.keys(e[entityType])) {
+			const value = e[entityType][key];
+			if (Array.isArray(value)) {
+				const newValue = [];
+				toArray(value).forEach(v => {
+					if (typeof v === "object") {
+						const nestedEntityType = Object.keys(v)[0];
+						v["schema/entity"] = `${nestedEntityType}-${counter++}`;
+						flattenedEntities.push(v);
+						newValue.push(v["schema/entity"]);
+					} else {
+						newValue.push(v);
+					}
+				});
+				e[entityType][key] = newValue;
+			} else if (typeof value === "object") {
+				const nestedEntityType = Object.keys(value)[0];
+				value["schema/entity"] = `${nestedEntityType}-${counter++}`;
+				flattenedEntities.push(value);
+				e[entityType][key] = value["schema/entity"];
+			}
+		}
+	});
+
+	return toArray(flattenedEntities).map(e => {
+		const entityType = Object.keys(e)[0];
+		const entityId = e["schema/entity"];
 		const entity: any = {
 			"schema/entity-type": entityType,
 		};
+		if (entityId) {
+			entity["schema/entity"] = entityId;
+		}
 		for (const key of Object.keys(e[entityType])) {
 			entity[`${entityType.replace(/\//, ".")}/${key}`] =
 				e[entityType][key];
