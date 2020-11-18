4e21e48266495c912fac1331f99c24e9f5508ed7
Christian Dupuis <cd@atomist.com>
2020-11-18 17:25:52 +0100

    Add support for nesting

--- test/transact.test.ts
+++ test/transact.test.ts
@@ -9,16 +9,46 @@ describe("transact", () => {
 					image:
 						"gcr.io/atomist-container-registry/pochta:3ec0bb7bab4012119d168263c79677ddb3084c48",
 					sha: "3ec0bb7bab4012119d168263c79677ddb3084c48",
+					labels: [
+						{
+							"docker.image/label": {
+								name: "author",
+								value: "Atomist",
+							},
+						},
+						{
+							"docker.image/label": {
+								name: "version",
+								value: "1.0.0",
+							},
+						},
+					],
 				},
 			};
 			const result = flattenEntities(entity);
 			assert.deepStrictEqual(result, [
 				{
-					"schema/entity-type": "docker/image",
 					"docker.image/image":
 						"gcr.io/atomist-container-registry/pochta:3ec0bb7bab4012119d168263c79677ddb3084c48",
+					"docker.image/labels": [
+						"docker.image/label-0",
+						"docker.image/label-1",
+					],
 					"docker.image/sha":
 						"3ec0bb7bab4012119d168263c79677ddb3084c48",
+					"schema/entity-type": "docker/image",
+				},
+				{
+					"docker.image.label/name": "author",
+					"docker.image.label/value": "Atomist",
+					"schema/entity": "docker.image/label-0",
+					"schema/entity-type": "docker.image/label",
+				},
+				{
+					"docker.image.label/name": "version",
+					"docker.image.label/value": "1.0.0",
+					"schema/entity": "docker.image/label-1",
+					"schema/entity-type": "docker.image/label",
 				},
 			]);
 		});
