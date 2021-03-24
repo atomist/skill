4964b362e13b5673ae6465e4f4ba4edd75957728
Christian Dupuis <cd@atomist.com>
2021-03-24 11:01:25 +0100

    Link changed files

--- lib/github/pull_request.ts
+++ lib/github/pull_request.ts
@@ -203,7 +203,12 @@ async function ensurePullRequest(
 
 ${files.length === 1 ? "File" : "Files"} changed:
 
-${files.map(f => `-   \`${f}\``).join("\n")}
+${files
+	.map(
+		f =>
+			`-   [\`${f}\`](https://github.com/${project.id.owner}/${project.id.repo}/blob/${push.branch}/${f})`,
+	)
+	.join("\n")}
 
 <!-- atomist:hide -->
 ---
