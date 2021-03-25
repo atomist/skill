f87263af7faedf2599d0479e79805f4b8457c001
Christian Dupuis <cd@atomist.com>
2021-03-25 18:39:00 +0100

    Fix isSubscriptionIncoming

--- lib/payload.ts
+++ lib/payload.ts
@@ -33,7 +33,7 @@ export function isSubscriptionIncoming(
 	// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 	event: any,
 ): event is SubscriptionIncoming {
-	return !!event.subscription;
+	return !!event.subscription && !event.data;
 }
 
 export function workspaceId(
