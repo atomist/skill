3da85ddb0bfcfcdf495c6c4e7f5913cf4ab33979
Christian Dupuis <cd@atomist.com>
2021-04-22 16:53:40 +0200

    Delint

--- lib/storage/resolver.ts
+++ lib/storage/resolver.ts
@@ -21,15 +21,15 @@ import * as path from "path";
 import { guid } from "../util";
 
 export const GoogleCloudStoragePayloadResolver = {
-	supports: (rawUrl: string) => rawUrl.startsWith("gs://"),
-	resolve: async (rawUrl: string) => {
+	supports: (rawUrl: string): boolean => rawUrl.startsWith("gs://"),
+	resolve: async (rawUrl: string): Promise<any> => {
 		const url = new URL(rawUrl);
 		const targetFilePath = path.join(os.tmpdir() || "/tmp", guid());
 		await fs.ensureDir(path.dirname(targetFilePath));
 		const storage = new (await import("@google-cloud/storage")).Storage();
 		const file = storage.bucket(url.host).file(url.pathname.slice(1));
 		await file.download({ destination: targetFilePath });
-		// await file.delete();
+		await file.delete();
 		return fs.readJson(targetFilePath);
 	},
 };
