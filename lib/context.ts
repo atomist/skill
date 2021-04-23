cd515302ff1913463fa4e8846e604fa6b4e1814b
Christian Dupuis <cd@atomist.com>
2021-04-23 14:45:14 +0200

    Polish

--- lib/context.ts
+++ lib/context.ts
@@ -232,7 +232,7 @@ export function createContext(
 	);
 	const rt = runtime();
 	debug(
-		"Starting %s/%s:%s %s - %s (%s) / %s",
+		"Starting %s/%s:%s '%s' - %s (%s) / %s",
 		payload.skill.namespace,
 		payload.skill.name,
 		payload.skill.version,
