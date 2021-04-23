4dec102548d484dbe29c97ab2bf7eb0fc73e82ed
Christian Dupuis <cd@atomist.com>
2021-04-23 18:41:58 +0200

    Only init logging for skills

--- lib/context.ts
+++ lib/context.ts
@@ -217,31 +217,33 @@ export function createContext(
 			onComplete,
 		};
 	}
-	initLogging(
-		{
-			skillId: payload.skill.id,
-			eventId: ctx.eventId,
-			correlationId: context.correlationId,
-			workspaceId: wid,
-		},
-		onComplete,
-		{
-			name: context.name,
-			skill: `${payload.skill.namespace}/${payload.skill.name}@${payload.skill.version}`,
-		},
-	);
-	const rt = runtime();
-	debug(
-		"Starting %s/%s:%s '%s' %satomist/skill:%s (%s) nodejs:%s",
-		payload.skill.namespace,
-		payload.skill.name,
-		payload.skill.version,
-		context.name,
-		rt.host?.sha ? `(${rt.host.sha.slice(0, 7)}) ` : "",
-		rt.skill.version,
-		rt.skill.sha.slice(0, 7),
-		rt.node.version,
-	);
+	if (context) {
+		initLogging(
+			{
+				skillId: payload.skill.id,
+				eventId: ctx.eventId,
+				correlationId: context.correlationId,
+				workspaceId: wid,
+			},
+			onComplete,
+			{
+				name: context.name,
+				skill: `${payload.skill.namespace}/${payload.skill.name}@${payload.skill.version}`,
+			},
+		);
+		const rt = runtime();
+		debug(
+			"Starting %s/%s:%s '%s' %satomist/skill:%s (%s) nodejs:%s",
+			payload.skill.namespace,
+			payload.skill.name,
+			payload.skill.version,
+			context.name,
+			rt.host?.sha ? `(${rt.host.sha.slice(0, 7)}) ` : "",
+			rt.skill.version,
+			rt.skill.sha.slice(0, 7),
+			rt.node.version,
+		);
+	}
 	return context;
 }
 
