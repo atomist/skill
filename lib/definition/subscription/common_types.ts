78f33ae4e08ff0386b2a5ccc945c78313224dd4b
Christian Dupuis <cd@atomist.com>
2021-04-29 10:33:11 +0200

    Add back title, description and cvssScore

--- lib/definition/subscription/common_types.ts
+++ lib/definition/subscription/common_types.ts
@@ -77,6 +77,9 @@ export interface BaseDockerVulnerability {
 }
 
 export interface DockerImageVulnerability extends BaseDockerVulnerability {
+	title: string;
+	description: string;
+	cvssScore: string;
 	fixed: Array<{ name: string; version: string }>;
 }
 
